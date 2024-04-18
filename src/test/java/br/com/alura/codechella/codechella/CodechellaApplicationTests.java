b9a3a4f955df36f37b64419c5607a627c4763071
goethecarvalho <goethecarvalho@gmail.com>
2024-04-18 15:22:02 -0300

    Arquitetura Java - Clean Architecture: Criando uma classe de configuração

--- src/test/java/br/com/alura/codechella/codechella/CodechellaApplicationTests.java
+++ src/test/java/br/com/alura/codechella/codechella/CodechellaApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.alura.codechella;
+package br.com.alura.codechella.codechella;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/br/com/alura/codechella/codechella/domain/entities/usuario/UsuarioTest.java
+++ src/test/java/br/com/alura/codechella/codechella/domain/entities/usuario/UsuarioTest.java
@@ -1,5 +1,7 @@
-package br.com.alura.codechella.domain.entities.usuario;
+package br.com.alura.codechella.codechella.domain.entities.usuario;
 
+import br.com.alura.codechella.domain.entities.usuario.FabricaDeUsuario;
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
