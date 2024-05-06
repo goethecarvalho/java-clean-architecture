b717032339697b1ad8c7a116f4cf03c6a048c57e
goethecarvalho <goethecarvalho@gmail.com>
2024-05-06 09:35:33 -0300

    Arquitetura Java - Clean Architecture: Persistindo dados em outro formato

---
+++ src/main/java/br/com/alura/codechella/UtilizaUsuariosComArquivos.java
@@ -0,0 +1,25 @@
+package br.com.alura.codechella;
+
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
+import br.com.alura.codechella.infra.gateways.RepositorioDeUsuarioEmArquivo;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+public class UtilizaUsuariosComArquivos {
+
+    public static void main(String[] args) {
+        RepositorioDeUsuarioEmArquivo repositorioDeUsuarioEmArquivo = new RepositorioDeUsuarioEmArquivo();
+
+        repositorioDeUsuarioEmArquivo.cadastrarUsuario(new Usuario("123.456.789-01", "João", LocalDate.parse("2000-10-15"), "joao@email.com"));
+
+        repositorioDeUsuarioEmArquivo.cadastrarUsuario(new Usuario("123.456.789-01", "Maria", LocalDate.parse("2000-10-15"), "maria@email.com"));
+
+        repositorioDeUsuarioEmArquivo.cadastrarUsuario(new Usuario("123.456.789-01", "Pedro", LocalDate.parse("2000-10-15"), "pedro@email.com"));
+
+        repositorioDeUsuarioEmArquivo.cadastrarUsuario(new Usuario("123.456.789-01", "Ana", LocalDate.parse("2000-10-15"), "ana@email.com"));
+
+        //System.out.println(repositorioDeUsuarioEmArquivo.listarTodos());
+        repositorioDeUsuarioEmArquivo.gravaEmArquivo("usuarios.txt");
+    }
+}
